83910d4a611cd0c849371dc0cdf731c22d07429d
Nitya agrawal <122293479+Nitya-34@users.noreply.github.com>
2025-06-20 12:14:38 +0530

    Update model_utils.py

--- weather_prediction_app/model_utils.py
+++ weather_prediction_app/model_utils.py
@@ -7,10 +7,10 @@ from joblib import load
 @st.cache_resource
 def load_models():
     return {
-        "XGBoost": pickle.load(open("models/xgb_model.pkl", "rb")),
-        "Random Forest": load("models/rf_model.joblib"),
-        "Linear Regression": pickle.load(open("models/lr_model.pkl", "rb")),
-        "SVR": pickle.load(open("models/svr_model.pkl", "rb"))
+        "XGBoost": pickle.load(open("weather_prediction_app/models/xgb_model.pkl", "rb")),
+        "Random Forest": load("weather_prediction_app/models/rf_model.joblib"),
+        "Linear Regression": pickle.load(open("weather_prediction_app/models/lr_model.pkl", "rb")),
+        "SVR": pickle.load(open("weather_prediction_app/models/svr_model.pkl", "rb"))
     }
 
 def make_prediction(model, input_df):
